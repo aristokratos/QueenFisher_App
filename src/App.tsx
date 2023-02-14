44a0c7891c2c5ce0eaac8724787fd480422d6591
Okoro Nnamdi <okoronnamdi4044@gmail.com>
2023-02-14 21:00:14 +0100

    Sidebar and Navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeTimetable from '../src/pages/RecipeTimetable';
+
 function App() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={(<RecipeTimetable />) as React.ReactElement}
+          />
+        </Routes>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
