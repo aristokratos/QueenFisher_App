0ad3f425a2c243dda8141c3e5c6cb280575a9ea0
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-02-14 14:20:06 +0100

    register-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Register from '../src/components/register/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+      <BrowserRouter>
+        <Routes>
+            <Route path="register" element={ <Register/>}/>
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
