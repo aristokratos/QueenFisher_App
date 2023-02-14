183ba45e50a07ae358ae1200b963fc1390d4fa07
Ifeanyi-M <gothikstormborn@gmail.com>
2023-02-14 15:48:04 +0100

    Landing Page Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landingPage/LandingPage';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index={true} element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
