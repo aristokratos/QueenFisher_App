9ed244dfaeb4099ba6afd8ddb436c4afd0d27603
DORIS-DUNU <dunudoris3@gmail.com>
2023-02-15 09:39:59 -1000

    Timetable and CreateTable

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,11 @@ const App: React.FC = () => {
     <React.Fragment>
     <Router>
       <Routes>
-
         <Route  path="/timetable" element={(<TimeTable />) as React.ReactElement} />
         <Route  path="/create" element={(<CreateTable />) as React.ReactElement} />
         <Route  path="/Continue" element={(<ConfirmDetails />) as React.ReactElement} />
         <Route index={true} element={<LandingPage />} />
         <Route path="register" element={ <Register/>}/>
-    
       </Routes>
     </Router>
     </React.Fragment>
