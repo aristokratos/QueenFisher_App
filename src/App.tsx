44860503f64384a82a4b5bb402d89f1a0b108fdb
GbadamosiIE <100071560+GbadamosiIE@users.noreply.github.com>
2023-02-15 08:33:46 +0100

    local Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-//import ForgetPassword from "./ForgetPasword/ForgetPassword";
-import SignUpManually from "./SignUpManually/SignUpManually";
-//import SignUpManually from "./SignUpManually/SignUpManually";
+
 
 function App() {
-  return <div><SignUpManually/></div>;
+  return <div></div>;
 }
 
 export default App;
