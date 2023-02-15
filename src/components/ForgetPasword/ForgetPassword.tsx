92cd9210490c2cbfe05811fe9642b16cd2b564ab
GbadamosiIE <100071560+GbadamosiIE@users.noreply.github.com>
2023-02-15 16:01:00 +0100

    Resolved conflict

--- src/components/ForgetPasword/ForgetPassword.tsx
+++ src/components/ForgetPasword/ForgetPassword.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import { CiMail } from 'react-icons/ci';
 import LogoImg from './LogoImg.png'
 import '../ForgetPasword/ForgetPass.css';
+import { Link } from 'react-router-dom';
 const ForgetPassword = () => {
   const [emailAddress, setEmailAddress] = useState("")
   const [Error, setError] = useState("")
@@ -43,35 +44,36 @@ const ForgetPassword = () => {
 
     <>
       <div>
-        <div className='header'>
-          <img src={LogoImg} className="logo" />
-          <h2 className='header-name'>
+        <div className='headers'>
+          <img src={LogoImg} className="logos" />
+          <h2 className='headername'>
             Queenfisher
           </h2>
         </div>
-        <div className='wrapper'>
+        <div className='wrappers'>
 
-          <div className='register-title'>
+          <div className='title'>
             Forget Password
           </div>
           <h3>Enter the email associated with your account and we'll send an email with instruction to reset your password</h3>
-          <div className='register-field-container'>
+          <div className='registercontainer'>
             {Error && (<div className="forgot-password-error-message">{Error}</div>)}
 
             <form method='Post' onSubmit={handleSubmit}>
 
-              <div className='registration-email'>
+              <div className='registrationemail'>
                 <div className='label-details'>
                   <label >Email</label>
                 </div>
-                <input className='input-field' value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} type="email" placeholder='Type your email' required />
-                <div className='register-icon'><CiMail /></div>
+                <input className='inputfield' value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} type="email" placeholder='Type your email' required />
+                <div className='registericon'><CiMail /></div>
               </div>
 
-              <div className='btn-div'>
-                <button className='registration-btn' type='submit'>Reset Password</button>
+              <div className='btndiv'>
+                <button className='registrationbtn' type='submit'>Reset Password</button>
 
               </div>
+              <Link className='link' to='./'>Back To Login</Link>
 
             </form>
 
