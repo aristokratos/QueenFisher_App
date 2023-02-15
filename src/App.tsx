4c9c812cd04012e639913e8ec8ac05bbd78cc3c7
DORIS-DUNU <dunudoris3@gmail.com>
2023-02-15 09:26:43 -1000

    Timetable and CreateTable

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TimeTable from './Timetable/Timetable';
 import CreateTable from './CreateTable/Createtable';
 import { ConfirmDetails } from './ConfirmDetails/ConfirmDetails';
+import LandingPage from './pages/landingPage/LandingPage';
+import Register from '../src/components/register/Register';
+
 
 const App: React.FC = () => {
   return (
@@ -10,14 +13,17 @@ const App: React.FC = () => {
     <Router>
       <Routes>
 
-        <Route  path="/" element={(<TimeTable />) as React.ReactElement} />
+        <Route  path="/timetable" element={(<TimeTable />) as React.ReactElement} />
         <Route  path="/create" element={(<CreateTable />) as React.ReactElement} />
         <Route  path="/Continue" element={(<ConfirmDetails />) as React.ReactElement} />
+        <Route index={true} element={<LandingPage />} />
+        <Route path="register" element={ <Register/>}/>
     
       </Routes>
     </Router>
     </React.Fragment>
   );
 };
+
 
 export default App;
---
+++ src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './landingpage.css';
+import MainHeader from '../../components/MainHeader';
+import MainHeader2 from '../../components/MainHeader2';
+import Values from '../../components/Values';
+import Choose from '../../components/Choose';
+import FirstFooter from '../../components/FirstFooter';
+
+import LastFooter from '../../components/LastFooter';
+import LandingNavbar from '../../components/LandingNavbar';
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <LandingNavbar/>
+      <MainHeader />
+      <MainHeader2 />
+      <Values />
+      <Choose />
+      <FirstFooter />
+      <LastFooter />
+    </>
+  );
+};
+
+export default LandingPage;
