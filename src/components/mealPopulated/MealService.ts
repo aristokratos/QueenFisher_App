480b3665a9c95fc6973473f2f6f9b0db43d9e11f
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-03-19 20:29:09 -0700

    integrate endpoint to get meals based on the timetable form fields with dummy api address

--- src/components/mealPopulated/MealService.ts
+++ src/components/mealPopulated/MealService.ts
@@ -1,7 +1,7 @@
 import type { IMeal } from './mealType';
 
 export const MealService = {
-  baseURL: 'http://dummy-api-address.com',
+  baseURL: 'http://dummy-api-address1.com',
 
   async getMealsByTimetable(fields: Record<string, string>): Promise<IMeal[]> {
     const { day, time } = fields;
