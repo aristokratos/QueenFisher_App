93611de10db2ff6a007b08b10f15dbf95fe6a217
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-03-27 18:59:35 -0700

    configuration updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // set chunk size limit to 2000kb to avoid warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // manually specify chunks to improve chunking
+        },
+      },
+    },
+  },
 });
